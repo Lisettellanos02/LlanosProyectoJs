b336b2b73912ad5dbfd76f406d686c1394568299
Lisettellanos02 <llanoslisette215.com>
2023-05-20 18:08:52 -0300

    grabo actualizaciones en html y css

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+var swiper = new swiper(".my swiper-1",{
+    slidesPerView:1,
+    spaceBetween:30,
+    loop:true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+tabInputs.forEach(function(input){
+    input.addEventListener('change', function(){
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+
+    })
+});
+
+var swiper = new swiper(".my swiper-2",{
+    slidesPerView:3,
+    spaceBetween:20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+    },
+    breakpoints :{
+        0:{
+            slidesPerView:1,
+        },
+        520:{
+            slidesPerView:2,
+        },
+        950:{
+            slidesPerView:3,
+        }
+    }
+});
+
+//colocar nuestros datos
 let nombre = prompt ("ingrese su nombre");
 let apellido = prompt ("ingrese su apellido");
 
@@ -7,61 +53,98 @@ let apellido = prompt ("ingrese su apellido");
     alert("Nombre y Apellido son requeridos");
 } 
 
-let respuesta = prompt ("¿Tenes pensado aprender otro idioma?").toLowerCase();
+//si quiero o no comprar un celular
+let respuesta = prompt ("¿Quieres comprar un celular?").toLowerCase();
 if (respuesta === "si"){
-alert ("Podes incribirte ya mismo a nuestra Institucion de Lenguas");
+alert ("Aqui encontraras las mejores promociones");
 }else{
-    alert("Lo esperamos cuando quiera comenzar con este curso");
+    alert("Lo esperamos cuando quieras comprar un dispositivo movil");
 }
 
-let usuario = prompt("Ingrese el usuario");
-while(usuario != "idiomas"){
-    alert("Usuario incorrecto");
-    usuario = prompt("Ingrese el usuario");
-}
-alert("Inscripciones abiertas");
-
-
-let lengua = prompt ("¿Que idioma quisieras aprender?");
-let modalidad = prompt ("Modalidad del cursado ¿virtual o presencial?");
-
- if (lengua != "" && modalidad != ""){
-    alert (`Cupos disponibles de inscripcion`);
- }else{
-    alert(`"Lengua y Modalidad son requeridos"`);
-} 
-
-
-for(let comision =1; comision <= 7; comision++){
-    let DNI = prompt ("Inscribite escribiendo tu DNI");
-    alert (`Comision #${comision} DNI: ${DNI}`);
+//producto disponible o no disponible
+const dispositivos = ["samsungS23", "samsungS22 ultra", "pantalon"];
+let marca = prompt ("ingrese el nombre del producto a buscar");
+let encontrado = productos.includes(marca);
+if(encontrado){
+    alert("Producto disponinble");
+}else{
+    alert("Producto no disponible");
 }
 
-function sumar(ingles, italiano, frances){
-    console.log(ingles + italiano + frances);
 
+//precio y modelos de celulares
+class Producto{
+    constructor(nombre, precio, imagen){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.vendido = false;
+    }
+    vender(){
+        this.vendido = true;
+    }
 }
-let resultado = sumar(35, 17, 15);
-console.log("Total de alumnos incriptos ",resultado);
+const producto1 = new Producto("Samsung J7", "178000", "htpp:asdf")
+const producto2 = new Producto("Samsung S20 FE", "230000", "htpp:asgd")
+const producto3 = new Producto("Samsung A70", "56000", "htpp:asud")
+const producto4 = new Producto("Samsung S23", "350000", "htpp:asbf")
+const producto5 = new Producto("Samsung A50 Plus", "450000", "htpp:rsdf")
+const producto6 = new Producto("Samsung S22 Ultra", "280000", "htpp:aedf")
+const producto7 = new Producto("Samsung S23 Ultra", "160000", "htpp:asof")
+console.log(producto1);
+producto1.vender();
+console.log(producto1);
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
+console.log(producto5);
+console.log(producto6);
+console.log(producto7);
 
-
-let idioma = "es";
-switch(idioma){
-    case"es":
-        console.log("idioma español");
-        break;
-
-    case "en":
-        console.log("idioma ingles");
-        break;
-
-    case "it":
-        console.log("idioma italiano");
-        break;      
-
-    default:
-        case "fr":
-            console.log("idioma frances");
-            break;
+//pesonas que tambien compraron 
+function Persona(info){
+    this.nombre = info.nombre;
+    this.edad = info.edad;
+    this.direccion = info.direccion;
 }
+const persona1 = new Persona({
+nombre: "Ana Paula Rossales",
+edad: 30,
+direccion: "Cordoba Capital",
+});
+const persona2 = new Persona({
+nombre: "Luis Cabrera",
+edad: 55,
+direccion: "Av. Juan.B Justo 334",
+});
+const persona3 = new Persona({
+nombre: "Jose Ernesto Diaz",
+edad: 43,
+direccion: "Av. Velez Sarfield 46",
+});
+const persona4 = new Persona({
+nombre: "Lucas Miretti",
+edad: 28,
+direccion: "Av Los Sauces 500",
+});
 
+console.log(persona1);
+console.log(persona2);
+console.log(persona3);
+console.log(persona4);
+
+//calcular intereses
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const iva = (x) => x *0.21;
+
+let precioproducto = 1000;
+let descuento = 10;
+
+let precioNuevo = resta (suma(precioproducto, iva(precioproducto)), descuento);
+console.log(nuevoPrecio);
+
+//agrego otro elemento
+const productos = ["celular", "cargador", "vidrio templado y funda"];
+productos.splice(1, 0, "auriculares inalambricos");
+console.log(productos);
